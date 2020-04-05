ef9a0d4cb4e5cf56326b5fc1cfdb6098b82c2df9
nvchaitanya <chathan2569@gmail.com>
2020-04-05 21:56:29 +0530

    updated

--- ToDo/src/components/Add.js
+++ ToDo/src/components/Add.js
@@ -13,15 +13,26 @@ class Add extends React.Component{
     }
 
     handleClick=()=>{
-        this.props.addfunc(this.state.itemname)
+        if(this.state.itemname===""){
+            alert('Please Fill the Input!')
+        } 
+        else{
+            this.props.addfunc(this.state.itemname)
+        }
+        this.reset()
     }
 
+    reset=()=>{
+        this.setState({
+            itemname:''
+        })
+    }
     render(){
         return(
             <div className="container">
                 <div className="row my-5 text-center">
                     <div className="col-10 offset-1 offset-md-0 col-md-6 col-lg-4 offset-lg-4 d-flex">
-                        <input type="text" className="form-control mr-2" placeholder="Add a Todo Item" onChange={this.handleChange} name="itemname"/>
+                        <input type="text" className="form-control mr-2" placeholder="Add a Todo Item" value={this.state.itemname} onChange={this.handleChange} name="itemname"/>
                         <button className="btn btn-primary" onClick={this.handleClick}>ADD</button>
                     </div>
                 </div>
